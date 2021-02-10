678f4f866c1e1aa080d2c073829b38c700d36ebf
mewiemers <m.wiemers01@gmail.com>
2021-02-10 17:33:47 +0100

    add filter option

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -71,8 +71,10 @@ export async function getCharacter(id: number) {
   const character = convertToCharacter(result);
   return character;
 }
-export async function getCharacters() {
-  const response = await fetch(`https://rickandmortyapi.com/api/character/`);
+export async function getCharacters(name?: string) {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/${name ? `?name=${name}` : ""}`
+  );
   const result = (await response.json()) as APICharacters;
   const characters = result.results.map((apiCharacter) =>
     convertToCharacter(apiCharacter)
